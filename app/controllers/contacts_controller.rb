07e74f1eb10e2b660a4643dd7779365c228b3a33
Greg Bell <zhongruige@gmail.com>
2015-11-20 03:58:11 +0000

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_parms) # create a new object, but filling out attributes with the contact_params from form
+        
+        if @contact.save # this commits it to the db if save works, otherwise it will run the else statement if save fails
+            flash[:success] = "Message sent." # Giving flash message a key (:success), the message is the value. This then gets passed in through application.html.erb
+            redirect_to new_contact_path # loads the contact form page again once saved successuflly
+        else
+            flash[:danger] = "Error occured, message has not been sent."
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        def contact_parms
+            params.require(:contact).permit(:name, :email, :comments) # done for security reasons, it's whitelisting these params
+        end
 end
