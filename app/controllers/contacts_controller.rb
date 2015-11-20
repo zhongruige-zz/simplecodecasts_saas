8a3a301f7762925195765edfd5037dc6642cb7ce
Greg Bell <zhongruige@gmail.com>
2015-11-20 04:19:08 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,14 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_parms) # create a new object, but filling out attributes with the contact_params from form
         
         if @contact.save # this commits it to the db if save works, otherwise it will run the else statement if save fails
+        
+            # Accessing the params hash and able to get those values, which it grabs from the form
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name, email, body).deliver
+            
             flash[:success] = "Message sent." # Giving flash message a key (:success), the message is the value. This then gets passed in through application.html.erb
             redirect_to new_contact_path # loads the contact form page again once saved successuflly
         else
