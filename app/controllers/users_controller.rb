55668c4b30047b2e2c9167926f618c4a382db3e4
Greg Bell <zhongruige@gmail.com>
2015-11-27 09:49:21 +0000

    Added users show action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id]) # Based on what gets passed through the URL, from the routes file (rake routes)
+    end
+end
