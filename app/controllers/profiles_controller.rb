e258fe4f03ed617259d42fbd5820ded0cf721f05
Greg Bell <zhongruige@gmail.com>
2015-11-27 19:26:59 +0000

    Changed new action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     def new # needs to match the file name under the views folder. Need to create a folder for each view, so for this: profiles > new.html.erb
         # form where a user can fill out their own profile
         @user = User.find(params[:user_id]) # find the user that is currently logged in grabbing it form the URL
-        @profile = @user.build_profile # We have this method because we established the model associations between users and profiles
+        @profile = Profile.new
     end
     
     def create
