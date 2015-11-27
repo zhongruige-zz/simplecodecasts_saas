f7ded416ca4edfb4b22962f179de6643049df980
Greg Bell <zhongruige@gmail.com>
2015-11-27 21:02:59 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+    
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new # needs to match the file name under the views folder. Need to create a folder for each view, so for this: profiles > new.html.erb
         # form where a user can fill out their own profile
         @user = User.find(params[:user_id]) # find the user that is currently logged in grabbing it form the URL
@@ -35,5 +39,10 @@ class ProfilesController < ApplicationController
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+        def only_current_user
+            @user = User.find(params[:user_id])
+            redirect_to(root_url) unless @user == current_user
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    
+    before_action :authenticate_user!
+    
     def show
         @user = User.find(params[:id]) # Based on what gets passed through the URL, from the routes file (rake routes)
     end
